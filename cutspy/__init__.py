47a07b98a2df62ef84fbca1fbf0c9d6774ddb7ac
mgualpa <mgualpa@gmail.com>
2024-10-28 12:19:21 -0300

    Add pyproject.toml and tox.ini. Clean __init__.py

--- cutspy/__init__.py
+++ cutspy/__init__.py
@@ -1,20 +1,3 @@
 # __init__.py
 
-from .cutspy_dom import (
-    RenderableElement,
-    AbstractRender,
-    TypedDict,
-    Stock,
-    StocksList,
-    Part,
-    PartsList,
-    Cut,
-    PatternsList,
-    AbstractObjective,
-    MinimumCostObjective,
-    MaximizeIncomeObjective,
-    MinimizeScrapObjective,
-    AbstractMultiObjectiveList,
-    MultiObjectiveList,
-    CSPModel
-)
+from .cutspy_dom import *
